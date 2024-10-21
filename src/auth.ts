0d0c67f3ad13fba4e960e5091489b14097c483ee
Guilherme Vieira <guilhermevieira@ua.pt>
2024-10-21 01:18:15 +0100

    BOL-29 Fully implement sign in and sign out

--- src/auth.ts
+++ src/auth.ts
@@ -3,4 +3,21 @@ import Cognito from "next-auth/providers/cognito"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
 	providers: [Cognito],
+	callbacks: {
+		async jwt({ token, user, profile }) {
+			if (user && profile) {
+				token.email = user.email
+				token.name = profile['cognito:username'] as string;
+			}
+
+			return token;
+		},
+		async session({ session, token }) {
+			if (token) {
+				session.user.name = token.name as string;
+				session.user.email = token.email as string;
+			}
+			return session;
+		}
+	}
 })
